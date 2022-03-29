43617fb3851744a739d380b966023ebabb7f54db
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-03-30 01:40:35 +0600

    Marvel Header Section Complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='container'>
+            <div className='header-section p-3 col-12'>
+                <h1>Marvel Studio</h1>
+                <p>The City is Under Control- Choose Your Avengers Team </p>
+                <h2> Total Budget: 100 Million</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
